31636d3d7dc95663c8f8325478e3e605e431f3f2
alan <alan@10.18.103.20>
2015-11-11 16:47:13 -0700

    Add hard-coded pie chart logic

---
+++ Firewatch/src/firewatch/MainTabsController.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firewatch;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+/**
+ * FXML Controller class
+ *
+ * @author alan
+ */
+public class MainTabsController implements Initializable {
+    @FXML
+    private PieChart pieChart;
+    @FXML
+    private RadioButton pieCauseRadio;
+    @FXML
+    private ToggleGroup pieRadioGroup;
+    @FXML
+    private RadioButton pieWeatherRadio;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+        pieChart.setData(pieChartData);
+    }    
+
+    @FXML
+    private void pieGroupByCause(ActionEvent event) {
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+        pieChart.setData(pieChartData);
+    }
+
+    @FXML
+    private void pieGroupByWeather(ActionEvent event) {
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Megan", 323),
+                new PieChart.Data("David", 257),
+                new PieChart.Data("Aidan", 53),
+                new PieChart.Data("William", 122),
+                new PieChart.Data("Brandon", 230));
+        pieChart.setData(pieChartData);
+    }
+    
+}
