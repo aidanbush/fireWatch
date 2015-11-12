8333a0d9daade8a961f38b7003f3b9ff3439c1c5
aidanbush <aidandbush@gmail.com>
2015-11-11 17:15:06 -0700

    add connection, and parseResultSet, complete getRangeInclusive fixed properties

--- Firewatch/src/firewatch/DatabaseModel.java
+++ Firewatch/src/firewatch/DatabaseModel.java
@@ -6,12 +6,19 @@
 package firewatch;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,14 +26,26 @@ import java.util.List;
  */
 public class DatabaseModel {
     
+    Connection getConnection(String fileName) throws SQLException{
+        Connection con = null;
+        
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectSQLite.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        con = DriverManager.getConnection("jdbc:sqlite:FireWatch.sqlite");
+        
+        return con;
+    }
     
-    List<WildFire> getRangeInclusive(Connection con, Date start, Date end) throws SQLException{
+    List<WildFire> getRangeInclusive(Connection con, Date start, Date end) throws SQLException, ParseException{
         List<WildFire> fires = null;
         
         //sql query
         Statement stmt = null;
-        String query = "SELECT * FROM filename "
-                + "WHERE fire_start_date => start AND ex_fs_date =< end";
+        String query = "SELECT * FROM Wildfire "
+                +"WHERE fire_start_date => " +start +" AND ex_fs_date =< " +end;
         
         try{
             stmt = con.createStatement();
@@ -46,8 +65,42 @@ public class DatabaseModel {
         return fires;
     }
 
-    private List<WildFire> parseResultSet(ResultSet rs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private List<WildFire> parseResultSet(ResultSet rs) throws SQLException, ParseException {
+        List<WildFire> fires = new ArrayList<>();
+        
+        while (rs.next()) {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd",Locale.getDefault());
+            String fireNumber = rs.getString(1);
+            int year = rs.getInt(2);
+            String fireName = rs.getString(3);
+            double latitude = rs.getDouble(4);
+            double longitude = rs.getDouble(5);
+            double[] coordinates = {latitude, longitude};
+            
+            String cause = rs.getString(6);
+            String activity = rs.getString(7);
+            Date start = null, end = null;
+            
+            if (rs.getString(8) != null){
+                start = df.parse(rs.getString(8));
+            }
+            String condition = rs.getString(9);
+            
+            if (rs.getString(10) != null){
+                end = df.parse(rs.getString(10));
+            }
+            float size = rs.getFloat(11);
+            String sizeClass = rs.getString(12);
+            
+            WildFire fire;
+            fire = new WildFire(fireNumber, year, fireName,
+                    coordinates, size, sizeClass.charAt(0), start, end, cause,
+                    activity, condition);
+            
+            fires.add(fire);
+        }
+        rs.close();
+        return fires;
     }
     
 }
