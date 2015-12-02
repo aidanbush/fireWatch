6b5703e3876cb57ee38e7cfe23a2eea4b0498678
MeganBaluch <MeganBaluch@10.60.11.41>
2015-12-02 12:53:01 -0700

    Automatic chart update on tab click Change of map icon Lab for if theres no fire

--- Firewatch/src/firewatch/FireWatch.java
+++ Firewatch/src/firewatch/FireWatch.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 /**
@@ -34,5 +35,6 @@ public class FireWatch extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
     
 }
